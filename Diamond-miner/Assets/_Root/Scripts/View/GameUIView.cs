b68f539efe65ca2469340574f097343d1b6a0d64
Budgo <Budgo@mail.ru>
2022-08-25 14:10:59 +0300

    Правки

--- Diamond-miner/Assets/_Root/Scripts/View/GameUIView.cs
+++ Diamond-miner/Assets/_Root/Scripts/View/GameUIView.cs
@@ -7,10 +7,14 @@ namespace View
     public class GameUIView : MonoBehaviour
     {
         [SerializeField] private Button _pauseMenuButton;
+        [SerializeField] private Button _removeStoneButton;
+        [SerializeField] private Button _powerButton;
         [SerializeField] private TMP_Text _diamondСounts;
         [SerializeField] private TMP_Text _gameTime;
 
         public Button PauseMenuButton => _pauseMenuButton;
+        public Button RemoveStoneButton => _removeStoneButton;
+        public Button PowerButton => _powerButton;
         public TMP_Text GameTime => _gameTime;
         public TMP_Text DiamondСounts => _diamondСounts;
     }
