810ea1cb032cfc6bb1937e137e48e910d47f7281
Budgo <Budgo@mail.ru>
2022-08-26 11:35:01 +0300

    Добавлен класс бонуса

---
+++ Diamond-miner/Assets/_Root/Scripts/Models/Bonus.cs
@@ -0,0 +1,14 @@
+﻿namespace Models
+{
+    public class Bonus
+    {
+        public int RemoveStone;
+        public int PowerButton;
+
+        public Bonus(int removeStone, int powerButton)
+        {
+            RemoveStone = removeStone;
+            PowerButton = powerButton;
+        }
+    }
+}
